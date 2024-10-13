d58c07fe989448c635a421144a962f64d647ceca
Keith Noguchi <keith@noguchi.us>
2024-10-13 09:01:12 -0700

    fixup! fixup! fixup! fixup! fixup! fixup! ch02: Modern Javascript

--- ch02/index.js
+++ ch02/index.js
@@ -1,4 +1,21 @@
+function tag(literal, ...values) {
+	console.log("literal", literal);
+	console.log("values", values);
+
+	let result;
+	switch (literal[1]) {
+		case " plus ":
+			result = values[0] + values[1];
+			break;
+		case " minus ":
+			result = values[0] - values[1];
+			break;
+	}
+	return `${values[0]}${literal[1]}${values[1]} is ${result}`;
+}
+
 let a = 1;
 let b = 2;
-let string = `${a} + ${b} = ${a + b}`;
-console.log(string);
+let output = tag`What is ${a} plus ${b}?`;
+
+console.log(output);
