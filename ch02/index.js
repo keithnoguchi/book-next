450d2576528aef83a7a5e189f1cb128e60ffa369
Keith Noguchi <keith@noguchi.us>
2024-10-16 07:56:11 -0700

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! ch02: Modern Javascript

--- ch02/index.js
+++ ch02/index.js
@@ -1,3 +1,17 @@
+// ```shell
+// $ node ch02/index.js
+// [
+//   { id: 1, name: 'Olivia Smith' },
+//   { id: 2, name: 'Liam Johnson' },
+//   { id: 3, name: 'Noah Williams' },
+//   { id: 4, name: 'Emma Brown' },
+//   { id: 5, name: 'Oliver Jones' },
+//   { id: 6, name: 'Charlotte Garcia' },
+//   { id: 7, name: 'Elijah Miller' },
+//   { id: 8, name: 'Amelia Davis' },
+//   { id: 9, name: 'James Rodriguez' },
+//   { id: 10, name: 'Ava Martinez' }
+// ]
 async function fetchData(url) {
 	try {
 		const resp = await fetch(url);
