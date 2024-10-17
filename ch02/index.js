4e4b79fd806a03119730b9c1b339027fd6279a3a
Keith Noguchi <keith@noguchi.us>
2024-10-17 07:15:42 -0700

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! ch02: Modern Javascript

--- ch02/index.js
+++ ch02/index.js
@@ -1,25 +1,4 @@
-// ```shell
-// $ node ch02/index.js
-// [
-//   { id: 1, name: 'Olivia Smith' },
-//   { id: 2, name: 'Liam Johnson' },
-//   { id: 3, name: 'Noah Williams' },
-//   { id: 4, name: 'Emma Brown' },
-//   { id: 5, name: 'Oliver Jones' },
-//   { id: 6, name: 'Charlotte Garcia' },
-//   { id: 7, name: 'Elijah Miller' },
-//   { id: 8, name: 'Amelia Davis' },
-//   { id: 9, name: 'James Rodriguez' },
-//   { id: 10, name: 'Ava Martinez' }
-// ]
-async function fetchData(url) {
-	try {
-		const resp = await fetch(url);
-		const json = await resp.json();
-		console.log(json);
-	} catch (err) {
-		console.error(err);
-	}
-}
-
-fetchData("https://www.usemodernfullstack.dev/api/v1/users");
+const original = [1, 2, 3, 4];
+const multiplied = original.map((item) => item * 10);
+console.log(`original array: ${original}`);
+console.log(`multiplied array: ${multiplied}`);
