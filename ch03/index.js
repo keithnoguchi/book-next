c8085ba1a22a54e18d6917c7fa17e8c558257778
Keith Noguchi <keith@noguchi.us>
2024-11-21 15:52:13 -0800

    fixup! fixup! ch03: TypeScript

--- ch03/index.js
+++ ch03/index.js
@@ -7,3 +7,12 @@ console.log(`fruit: ${fruit}, color: ${color}`);
 color = "red";
 
 console.log(`object.color: ${object.color}, color: ${color}`);
+
+const originalArray = [1, 2, 3];
+const clonedArray = [...originalArray];
+
+clonedArray[0] = "one";
+clonedArray[1] = "two";
+clonedArray[2] = "three";
+
+console.log(`originalArray: ${originalArray}, clonedArray: ${clonedArray}`);
