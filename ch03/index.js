db293b1ac293fd3b5e216b6cbc96deb0146eb75d
Keith Noguchi <keith@noguchi.us>
2024-11-21 16:32:27 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! ch03: TypeScript

--- ch03/index.js
+++ ch03/index.js
@@ -1,18 +1,16 @@
-const object = { fruit: "apple", color: "green" };
-
-let { fruit, color } = { ...object };
-
-console.log(`fruit: ${fruit}, color: ${color}`);
-
-color = "red";
-
-console.log(`object.color: ${object.color}, color: ${color}`);
-
-const originalArray = [1, 2, 3];
-const clonedArray = [...originalArray];
-
-clonedArray[0] = "one";
-clonedArray[1] = "two";
-clonedArray[2] = "three";
-
-console.log(`originalArray: ${originalArray}, clonedArray: ${clonedArray}`);
+"use strict";
+let string = "1";
+let number = 1;
+let result;
+const calculate = (a, b) => {
+    if (Number.isInteger(a) === false || Number.isInteger(b) == false) {
+        throw new Error("Invalid type: a parameter is not an integer");
+    }
+    else {
+        return a + b;
+    }
+};
+result = calculate(number, number);
+console.log(`value: ${result}, type of ${typeof (result)}`);
+result = calculate(number, string);
+console.log(`value: ${result}, type of ${typeof (result)}`);
