c74f90542fabef3660892d37d81e1e0c37479992
Keith Noguchi <keith@noguchi.us>
2024-11-21 17:32:09 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! ch03: TypeScript

--- ch03/index.js
+++ ch03/index.js
@@ -1,15 +1,4 @@
-var string = "1";
-var number = 1;
-var result;
-var calculate = function (a, b) {
-    if (Number.isInteger(a) === false || Number.isInteger(b) == false) {
-        throw new Error("Invalid type: a parameter is not an integer");
-    }
-    else {
-        return a + b;
-    }
-};
-result = calculate(number, number);
-console.log("value: ".concat(result, ", type of ").concat(typeof (result)));
-result = calculate(number, string);
-console.log("value: ".concat(result, ", type of ").concat(typeof (result)));
+function log(msg) {
+    console.log(msg);
+}
+console.log("Hello TypeScript");
--- ch03/index.ts
+++ ch03/index.ts
@@ -1,17 +1,5 @@
-let string = "1";
-let number = 1;
-let result;
+function log(msg: string): void {
+	console.log(msg);
+}
 
-const calculate = (a, b) => {
-	if (Number.isInteger(a) === false || Number.isInteger(b) == false) {
-		throw new Error("Invalid type: a parameter is not an integer");
-	} else {
-		return a + b;
-	}
-};
-
-result = calculate(number, number);
-console.log(`value: ${result}, type of ${typeof(result)}`);
-
-result = calculate(number, string);
-console.log(`value: ${result}, type of ${typeof(result)}`);
+console.log("Hello TypeScript");
